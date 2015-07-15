e536dd24a6c15b98e389202d74ba8ff8191dbb59
Norcross <norcross@reaktivstudios.com>
2015-07-15 12:24:47 -0400

    fixed CSS placement, added function for blog category output

--- lib/front.php
+++ lib/front.php
@@ -12,12 +12,11 @@
  * load and display our post hero
  *
  * @param  integer $post_id  [description]
- * @param  string  $category [description]
  * @param  string  $classes  [description]
  * @param  boolean $echo     [description]
  * @return [type]            [description]
  */
-function csbh_post_hero( $post_id = 0, $category = '', $classes = '', $echo = true ) {
+function csbh_post_hero( $post_id = 0, $classes = '', $echo = true ) {
 
 	// check for a post ID if one wasn't passed
 	if ( empty( $post_id ) ) {
@@ -34,6 +33,10 @@ function csbh_post_hero( $post_id = 0, $category = '', $classes = '', $echo = tr
 		$post_id    = absint( $post->ID );
 	}
 
+	// fetch my category
+	$catobj = get_the_category( $post_id );
+	$cslug  = ! empty( $catobj[0] ) && ! empty( $catobj[0]->slug ) ? $catobj[0]->slug : '';
+
 	// fetch our meta
 	$meta   = get_post_meta( $post_id, '_csbh_meta', true );
 
@@ -45,7 +48,7 @@ function csbh_post_hero( $post_id = 0, $category = '', $classes = '', $echo = tr
 	if ( empty( $meta ) || empty( $meta['image'] ) ) {
 
 		// add the stuff we have
-		$html  .= 'class="' . $classes . ' hero--post--category hero--post--category--' . $category . '"';
+		$html  .= 'class="' . $classes . ' hero--post--category hero--post--category--' . $cslug . '"';
 
 		// echo if requested
 		if ( ! empty( $echo ) ) {
@@ -68,7 +71,7 @@ function csbh_post_hero( $post_id = 0, $category = '', $classes = '', $echo = tr
 	if ( empty( $size ) ) {
 
 		// set the style
-		$style .= 'background: ' . esc_attr( $color ) . ' url( ' . esc_url( $image ) . ' center ' . esc_attr( $repeat ) . ' );';
+		$style .= 'background: ' . esc_attr( $color ) . ' url( ' . esc_url( $image ) . ' ) center ' . esc_attr( $repeat ) . ';';
 	}
 
 	// we have a size, so do our check for repeats
@@ -78,19 +81,75 @@ function csbh_post_hero( $post_id = 0, $category = '', $classes = '', $echo = tr
 		if ( $repeat == 'repeat-x' && ! empty( $ximage ) ) {
 
 			// set the style
-			$style .= 'background: url( ' . esc_url( $image ) . ' center ' . esc_attr( $repeat ) . ' ), url( ' . esc_url( $ximage ) . ' center repeat-x );';
+			$style .= 'background: url( ' . esc_url( $image ) . ' ) center ' . esc_attr( $repeat ) . ', url( ' . esc_url( $ximage ) . ' ) center repeat-x;';
 			$style .= 'background-size: ' . esc_attr( $size ) . ', background-size: ' . esc_attr( $size ) . ';';
 
 		} else { // we had a normal one.
 
 			// set the style
-			$style .= 'background: url( ' . esc_url( $image ) . ' center ' . esc_attr( $repeat ) . ' );';
+			$style .= 'background: url( ' . esc_url( $image ) . ' ) center ' . esc_attr( $repeat ) . ';';
 			$style .= 'background-size: ' . esc_attr( $size ) . ';';
 		}
 	}
 
 	// set the HTML class, pulling in our style
-	$html  .= 'class="' . $classes . ' hero--post--custom ' . $medium . '" style="' . $style . '"';
+	$html  .= '<div class="' . $classes . ' hero--post--custom ' . $medium . '" style="' . $style . '">';
+		$html  .= '<div class="cell cell--m well well--l">';
+		// the `post_hero_image` thing goes here once I figure out how it works
+		$html  .= '</div>';
+	$html  .= '</div>';
+
+	// echo if requested
+	if ( ! empty( $echo ) ) {
+		echo $html;
+	}
+
+	// no echo. return
+	return $html;
+}
+
+/**
+ * fetch the category info and display it
+ *
+ * @param  integer $post_id  [description]
+ * @param  boolean $echo     [description]
+ * @return [type]            [description]
+ */
+function csbh_post_category_text( $post_id = 0, $echo = true ) {
+
+	// check for a post ID if one wasn't passed
+	if ( empty( $post_id ) ) {
+
+		// load the global
+		global $post;
+
+		// bail if it isn't there
+		if ( empty( $post ) || ! is_object( $post ) || empty( $post->ID ) ) {
+			return;
+		}
+
+		// set my post ID
+		$post_id    = absint( $post->ID );
+	}
+
+	// fetch my category
+	$catobj = get_the_category( $post_id );
+
+	// bail with no category data
+	if ( empty( $catobj ) || ! is_array( $catobj ) || empty( $catobj[0] ) ) {
+		return;
+	}
+
+	// get my category link
+	$link   = get_category_link( $catobj[0]->term_id );
+
+	// set an empty HTML block and style block
+	$html   = '';
+
+	// build it out
+	$html  .= '<em class="tcs"> in ';
+	$html  .= '<a class="bdrb tcs" href="' . esc_url( $link ) . '">' . esc_attr( $catobj[0]->name ) . '</a>';
+	$html  .= '</em>';
 
 	// echo if requested
 	if ( ! empty( $echo ) ) {
